c8db0cdc32f6892b7fe6c10c468135c9d64ad9d5
shaosh <shaosh0913@gmail.com>
2014-07-29 13:05:13 -0700

    Add app codes

---
+++ myapp/chatroom/app/js/controllers.js
@@ -0,0 +1,97 @@
+/* Controllers */
+
+function AppCtrl($scope, socket) {
+
+  // Socket listeners
+  // ================
+  socket.on('init', function (data) {
+    $scope.name = data.name;    
+    $scope.users = data.users;
+  });
+
+  socket.on('send:message', function (message) {
+    $scope.messages.push(message);
+  });
+
+  socket.on('change:name', function (data) {
+    changeName(data.oldName, data.newName);
+  });
+
+  socket.on('user:join', function (data) {
+    $scope.messages.push({
+      user: 'chatroom',
+      text: 'User ' + data.name + ' has joined.'
+    });
+    $scope.users.push(data.name);
+  });
+
+  // add a message to the conversation when a user disconnects or leaves the room
+  socket.on('user:left', function (data) {
+    $scope.messages.push({
+      user: 'chatroom',
+      text: 'User ' + data.name + ' has left.'
+    });
+    var i, user;
+    for (i = 0; i < $scope.users.length; i++) {
+      user = $scope.users[i];
+      if (user === data.name) {
+        $scope.users.splice(i, 1);
+        break;
+      }
+    }
+  });
+
+  // Private helpers
+  // ===============
+
+  var changeName = function (oldName, newName) {
+    // rename user in list of users
+    var i;
+    for (i = 0; i < $scope.users.length; i++) {
+      if ($scope.users[i] === oldName) {
+        $scope.users[i] = newName;
+      }
+    }
+
+    $scope.messages.push({
+      user: 'chatroom',
+      text: 'User ' + oldName + ' is now known as ' + newName + '.'
+    });
+  }
+
+  // Methods published to the scope
+  // ==============================
+
+  $scope.changeName = function () {
+    socket.emit('change:name', {
+      name: $scope.newName
+    }, function (result) {
+      if (!result) {
+        alert('There was an error changing your name');
+      } else {
+
+        changeName($scope.name, $scope.newName);
+
+        $scope.name = $scope.newName;
+        $scope.newName = '';
+      }
+    });
+  };
+
+  $scope.messages = [];
+
+  $scope.sendMessage = function () {
+    socket.emit('send:message', {
+      message: $scope.message
+    });
+
+    // add the message to our model locally
+    $scope.messages.push({
+      user: $scope.name,
+      text: $scope.message
+    });
+
+    // clear message box
+    $scope.message = '';
+  };
+}
---
+++ myapp/chatroom/app/js/services.js
@@ -0,0 +1,26 @@
+angular.module('myApp.services', [])
+    .value('version', '0.1')
+    .factory('socket', function($rootScope) {
+    /* 定位 socket IO 服务器, 括弧把 ip 弄上 */
+    var socket = io.connect("http://127.0.0.1:3000"); 
+    return {
+        on: function(eventName, callback) {
+            socket.on(eventName, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    callback.apply(socket, args);
+                });
+            });
+        },
+        emit: function(eventName, data, callback) {
+            socket.emit(eventName, data, function() {
+                var args = arguments;
+                $rootScope.$apply(function() {
+                    if (callback) {
+                        callback.apply(socket, args);
+                    }
+                });
+            })
+        }
+    };
+});
